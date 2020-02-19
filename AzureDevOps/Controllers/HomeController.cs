58b9777df42382166fd87b8cf0f94f16cb531e03
ELHADJ TAHIROU MINTHE <elhadjtahirou.minthe@gmail.com>
2020-02-19 10:19:18 +0100

    third commit

--- AzureDevOps/Controllers/HomeController.cs
+++ AzureDevOps/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace AzureDevOps.Controllers
 
         public IActionResult Privacy()
         {
-            return View()
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
